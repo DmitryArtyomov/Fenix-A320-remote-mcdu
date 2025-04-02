c86539bd9d1b319a5eee5af121609e76be9d6309
DmitryArtyomov <artyomovd@yandex.ru>
2025-04-03 00:12:46 +0300

    Fix styling. Add auto-reconnect. Code refactoring

--- index.js
+++ index.js
@@ -1,142 +1,182 @@
-const parseMcduXml = (string) => {
-    const xml = (new DOMParser).parseFromString(string, "text/xml");
-    const titles = xml.getElementsByTagName("title");
-    const lines = xml.getElementsByTagName("line");
-    const scratchpad = xml.getElementsByTagName("scratchpad");
-    const textFunc = (str) => (str.textContent == null) ? '' : str.textContent;
-    return [
-        ...Array.from(titles).map(textFunc),
-        ...Array.from(lines).map(textFunc),
-        ...Array.from(scratchpad).map(textFunc)
-    ];
-}
+class MCDU {
+    #lines = 14;
+    #columns = 24;
+    #charSizeWidthRatio = 14.85;
+    #maxLetterSpacing = 5;
 
-const isModifier = (str) => str.toLowerCase() === str && str !== str.toUpperCase();
-const modifierClass = {
-    "s": "small",
-    "l": "large",
-    "a": "amber",
-    "w": "white",
-    "c": "cyan",
-    "g": "green",
-    "m": "magenta",
-    "r": "red",
-    "y": "yellow"
-};
-const modifierClasses = (str, currentClasses) => {
-    const classes = Array.from(currentClasses);
-    [...str].map((char) => modifierClass[char]).filter(c => c).forEach((klass) => {
-        const idx = (klass == 'small' || klass == 'large') ? 0 : 1;
-        classes[idx] = klass;
-    });
-    return classes;
-}
+    constructor(remoteIp = 'localhost', remotePort = '8083', mcduIndex = '1') {
+        const remoteAddress = `${remoteIp}:${remotePort}`;
+        this.mcduIndex = mcduIndex;
 
-const renderLine = (line) => {
-    let classes = [null, null];
-    const content = line.split(/([a-z]+)/).filter((s) => s.length).map((part) => {
-        if (isModifier(part)) {
-            classes = modifierClasses(part, classes);
-            return `<i class="${classes.filter(c => c).join(' ')}"></i>`;
-        } else {
-            return `<span>${escapeHtmlEntities(part)}</span>`;
-        }
-    }).join('');
-    return `<div class="line">${content}</div>`;
-};
-const renderLines = (lines) => {
-    const contents = lines.map(renderLine).join("\n");
-    document.getElementById('mcdu-content').innerHTML = contents;
-};
+        this.initializeObserver();
+        this.initializeWebsocket(remoteAddress);
+    }
 
-const escapeHtmlEntities = (string) => {
-    const replacer = (match) => {
-        if (match === "&") return "&amp;";
-        else if (match === "<") return "&lt;";
-        else if (match === ">") return "&gt;";
-        else if (match === "'") return "&apos;";
-        else if (match === '"') return "&quot;";
+    modifierClass = {
+        "s": "small",
+        "l": "large",
+        "a": "amber",
+        "w": "white",
+        "c": "cyan",
+        "g": "green",
+        "m": "magenta",
+        "r": "red",
+        "y": "yellow"
     };
 
-    return string.replace(/[&<>'"]/g, replacer);
-}
+    parseXML(string) {
+        const xml = (new DOMParser).parseFromString(string, "text/xml");
+        const titles = xml.getElementsByTagName("title");
+        const lines = xml.getElementsByTagName("line");
+        const scratchpad = xml.getElementsByTagName("scratchpad");
+        const textFunc = (str) => (str.textContent == null) ? '' : str.textContent;
+        return [
+            ...Array.from(titles).map(textFunc),
+            ...Array.from(lines).map(textFunc),
+            ...Array.from(scratchpad).map(textFunc)
+        ];
+    }
 
-let socket, resizeObserver;
+    isModifier(str) {
+        return str.toLowerCase() === str && str !== str.toUpperCase();
+    }
 
-const sendMessage = (message) => {
-    socket.send(JSON.stringify(message));
-}
+    modifyClasses(str, currentClasses) {
+        [...str].map((char) => this.modifierClass[char]).filter(c => c).forEach((klass) => {
+            const idx = (klass == 'small' || klass == 'large') ? 0 : 1;
+            currentClasses[idx] = klass;
+        });
+    }
 
-const displayChanged = (ref) => {
-    const parsedLines = parseMcduXml(ref);
-    renderLines(parsedLines);
-}
+    renderLine(line) {
+        const classes = [null, null];
+        const content = line.split(/([a-z]+)/).filter((s) => s.length).map((part) => {
+            if (this.isModifier(part)) {
+                this.modifyClasses(part, classes);
+                return `<i class="${classes.filter(c => c).join(' ')}"></i>`;
+            } else {
+                return `<span>${this.escapeHtmlEntities(part)}</span>`;
+            }
+        }).join('');
+        return `<div class="line">${content}</div>`;
+    };
+    
+    renderScreen(lines) {
+        const contents = lines.map((line) => this.renderLine(line)).join("\n");
+        document.getElementById('mcdu-content').innerHTML = contents;
+    };
 
-const messageReceived = (message) => {
-    if (message.type == 'data' && message.id == '1' && message.payload?.data?.dataRefs?.name == 'aircraft.mcdu1.display') {
-        displayChanged(message.payload.data.dataRefs.value);
+    escapeHtmlEntities(string) {
+        const replacer = (match) => {
+            if (match === "&") return "&amp;";
+            else if (match === "<") return "&lt;";
+            else if (match === ">") return "&gt;";
+            else if (match === "'") return "&apos;";
+            else if (match === '"') return "&quot;";
+        };
+    
+        return string.replace(/[&<>'"]/g, replacer);
+    }
+    
+
+    showCustomMessage(text) {
+        const emptyLine = ' '.repeat(this.#columns);
+        const lines = text.split('\n').map((s) => s.trim()).map((line) => {
+            const modifiers = (line.match(/[a-z]/g) || []).length;
+            const textLength = line.length - modifiers;
+            const leftPadding = Math.floor((this.#columns - textLength) / 2);
+            const rightPadding = this.#columns - textLength - leftPadding;
+            return `${' '.repeat(leftPadding)}${line}${' '.repeat(rightPadding)}`
+        });
+
+        const topPadding = Math.floor((this.#lines - lines.length) / 2);
+        const bottomPadding = this.#lines - lines.length - topPadding;
+        const messageLines = [
+            ...Array(topPadding).fill(emptyLine),
+            ...lines,
+            ...Array(bottomPadding).fill(emptyLine)
+        ];
+        this.renderScreen(messageLines);
+    }
+
+    sendSocketMessage(message) {
+        this.socket.send(JSON.stringify(message));
+    }
+
+    messageReceived(message) {
+        if (message.type == 'data' && message.id == '1' && message.payload?.data?.dataRefs?.name == `aircraft.mcdu${this.mcduIndex}.display`) {
+            const value = message.payload.data.dataRefs.value;
+            const parsedLines = this.parseXML(value);
+            this.renderScreen(parsedLines);
+        }
+    }
+
+    initializeWebsocket(remoteAddress) {
+        this.showCustomMessage(`CONNECTING\nsTO\nsm${remoteAddress}`);
+        this.socket = new WebSocket(`ws://${remoteAddress}/graphql`, 'graphql-ws');
+        this.socket.addEventListener("message", (event) => {
+            this.messageReceived(JSON.parse(event.data));
+        });
+        this.socket.addEventListener("error", (event) => {
+            this.socket.close();
+        });
+        this.socket.addEventListener("close", (event) => {
+            this.showCustomMessage('DISCONNECTED\n\nsmATTEMPTING RECONNECT\nsmIN g1 mSECOND');
+            setTimeout(() => {
+                this.initializeWebsocket(remoteAddress);
+            }, 1000);
+        });
+        this.socket.addEventListener("open", (event) => {
+            this.sendSocketMessage({
+                type: "connection_init",
+                payload: {},
+            });
+            this.sendSocketMessage({
+                id: "1",
+                type: "start",
+                payload: {
+                    variables: {
+                        names: [`aircraft.mcdu${this.mcduIndex}.display`]
+                    },
+                    extensions: {},
+                    operationName: "OnDataRefChanged",
+                    query: "subscription OnDataRefChanged($names: [String!]!) {\n  dataRefs(names: $names) {\n    name\n    value\n    __typename\n  }\n}"
+                }
+            });
+        });
+    }
+
+    initializeObserver() {
+        this.resizeObserver = new ResizeObserver((entries) => {
+            for (const entry of entries) {
+                this.containerResized(entry);
+            }
+        });
+        const container = document.getElementById('container');
+        this.resizeObserver.observe(container);
+    }
+
+    containerResized(entry) {
+        const width = entry.contentRect.width;
+        const height = entry.contentRect.height;
+        const maxSizeByHeight = height / this.#lines;
+        const maxSizeByWidth = width / this.#charSizeWidthRatio;
+        const fontSize = Math.min(maxSizeByWidth, maxSizeByHeight);
+        const fontSizeRounded = Math.floor(fontSize);
+
+        const mcduEl = document.getElementById('mcdu-content');
+        mcduEl.style.setProperty("--font-size", `${fontSizeRounded}px`);
+
+        const excessWidth = width - fontSizeRounded * this.#charSizeWidthRatio;
+        const letterSpacing = Math.min(Math.floor(excessWidth / this.#columns), this.#maxLetterSpacing);
+        mcduEl.style.setProperty("--letter-spacing", `${letterSpacing}px`);
     }
 }
 
-const connectionSettings = () => {
+document.addEventListener('DOMContentLoaded', () => {
     const queryParams = new URLSearchParams(window.location.search);
     const ip = queryParams.get('ip') || 'localhost';
     const port = queryParams.get('port') || '8083';
-    return { ip: ip, port: port };
-}
-
-const initializeWebsocket = () => {
-    const settings = connectionSettings();
-    socket = new WebSocket(`ws://${settings.ip}:${settings.port}/graphql`, 'graphql-ws');
-    socket.addEventListener("message", (event) => {
-        messageReceived(JSON.parse(event.data));
-    })
-    socket.addEventListener("open", (event) => {
-        sendMessage({
-            type: "connection_init",
-            payload: {},
-        });
-        sendMessage({
-            id: "1",
-            type: "start",
-            payload: {
-                variables: {
-                    names: ["aircraft.mcdu1.display"]
-                },
-                extensions: {},
-                operationName: "OnDataRefChanged",
-                query: "subscription OnDataRefChanged($names: [String!]!) {\n  dataRefs(names: $names) {\n    name\n    value\n    __typename\n  }\n}"
-            }
-        });
-    });
-}
-
-const initializeObserver = () => {
-    resizeObserver = new ResizeObserver((entries) => {
-        for (const entry of entries) {
-            windowResized(entry);
-        }
-    });
-    const container = document.getElementById('container');
-    resizeObserver.observe(container);
-}
-
-const windowResized = (entry) => {
-    const width = entry.contentRect.width;
-    const height = entry.contentRect.height;
-    const maxSizeByHeight = height / 14.0;
-    const maxSizeByWidth = width / 14.85;
-    const fontSize = Math.min(maxSizeByWidth, maxSizeByHeight);
-    const fontSizeRounded = Math.floor(fontSize);
-    const mcduEl = document.getElementById('mcdu-content');
-    mcduEl.style.setProperty("--font-size", `${fontSizeRounded}px`);
-    const excessWidth = width - fontSizeRounded * 14.85;
-    const letterSpacing = Math.min(Math.floor(excessWidth / 24.0), 5);
-    mcduEl.style.setProperty("--letter-spacing", `${letterSpacing}px`);
-}
-
-document.addEventListener('DOMContentLoaded',function() {
-    initializeWebsocket();
-    initializeObserver();
+    const mcduIndex = queryParams.get('mcdu') || '1';
+    window.mcdu = new MCDU(ip, port, mcduIndex);
 })
-
